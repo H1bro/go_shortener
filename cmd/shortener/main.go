db5f59f1254bad359767ee81a7a285100d547406
H1Bro <1feyfk@gmail.com>
2022-05-31 10:08:15 +0300

    Sprint 1. Inkrement 1. First commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import "github.com/H1bro/yandex-practicum-go/internal/app/server"
+
+func main() {
+
+	serverAddr := "localhost:8080"
+
+	s := server.Server{Addr: serverAddr}
+	s.StartServer()
+}
---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/H1bro/go_shortener
+
+go 1.18
+
